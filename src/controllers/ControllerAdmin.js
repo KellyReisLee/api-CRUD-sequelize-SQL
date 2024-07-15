b564d55176bec792029cd43f747080cbcfe2d965
Kelly Reis Lee <kellyreislee@MBP-de-Kelly.home>
2024-07-15 13:44:41 +0100

    adding delete route

--- src/controllers/ControllerAdmin.js
+++ src/controllers/ControllerAdmin.js
@@ -10,16 +10,45 @@ exports.getAll = async (req, res, next) => {
 
 }
 
-exports.postEmployee = async (req, res, next) => {
 
+exports.getEmployee = async (req, res, next) => {
+  const { id } = req.params;
   try {
-    const user = await db.Employee.create({
-      name: req.body.name,
-      email: req.body.email,
-      gender: req.body.gender,
-      mobile: req.body.mobile
+    const employee = await db.Employee.findByPk(id);
+    if (!employee) {
+      return res.status(400).json({ message: 'User not found!' })
+    }
+    res.status(200).json(employee)
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+
+}
+
+
+
+exports.postEmployee = async (req, res, next) => {
+  const { name, email, gender, mobile } = req.body;
+  try {
+    // const user = await db.Employee.create({
+
+    //   // name: req.body.name,
+    //   // email: req.body.email,
+    //   // gender: req.body.gender,
+    //   // mobile: req.body.mobile
+    // });
+    const dataEmployee = await db.Employee.findOrCreate({
+      where: { email },
+      defaults: { name, email, gender, mobile },
     });
-    res.status(201).json(user);
+
+    if (dataEmployee[1] === true) {
+      return res.status(201).json({ message: 'Created Successfully', data: dataEmployee[0] })
+    } else {
+      return res.status(201).json({ message: 'This email already exist.' })
+    }
+
+
   } catch (error) {
     if (error.name) {
       const messages = error.errors.map(err => err.message);
@@ -28,4 +57,51 @@ exports.postEmployee = async (req, res, next) => {
     res.status(500).json({ error: error.message });
   }
 };
+
+
+
+exports.updateEmployee = async (req, res, next) => {
+  const { id } = req.params;
+  const newData = req.body;
+  try {
+    //const employee = await db.Employee.findByPk(id);
+
+    const data = await db.Employee.update(
+      { ...newData }, // Campos e novos valores
+      {
+        where: { id }, // Condição de onde atualizar
+      }
+    );
+
+    const newUser = await db.Employee.findByPk(id)
+    if (!data[0]) {
+      return res.status(400).json({ message: 'Data not found!' })
+    }
+
+    return res.status(200).json(newUser)
+
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+
+exports.deleteEmployee = async (req, res, next) => {
+  const { id } = req.params;
+  const employee = await db.Employee.findByPk(id);
+  try {
+    const deleteEmployee = await db.Employee.destroy({
+      where: { id }
+    });
+    if (!deleteEmployee) {
+      return res.status(400).json({ message: 'Could not delete user!' })
+    }
+    res.status(200).json({ message: `User with email: ${employee.email}  - was deleted successfully!` })
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+
+}
+
+
 
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -3,6 +3,9 @@ const admin = require('../controllers/ControllerAdmin')
 const router = Router();
 
 router.get("/", admin.getAll)
+router.get("/:id", admin.getEmployee)
 router.post("/add-employee", admin.postEmployee)
+router.put('/update-employee/:id', admin.updateEmployee)
+router.delete('/delete-employee/:id', admin.deleteEmployee)
 
 module.exports = router;
